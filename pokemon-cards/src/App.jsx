c0d07acf1fbbc981eda4295c1820053376d30cf2
gmohammedkaif <gmohammedkai@gmail.com>
2026-03-17 12:37:41 +0530

    fixed case issue

--- pokemon-cards/src/App.jsx
+++ pokemon-cards/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Pokemon from './Pokemon'
-import Pokemonn from './Pokemonn'
+
 
 const App = () => {
   return (
     <div>
       <Pokemon />
-      {/* <Pokemonn /> */}
+    
     </div>
   )
 }
